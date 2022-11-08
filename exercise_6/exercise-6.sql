57fcef7eb464a31d0e72ca9e0e59f170a8b23c89
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-08 12:27:37 -0800

    6 complete

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,13 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+select title
+
+from film left join inventory
+ON film.film_id = inventory.film_id
+
+left join rental
+on rental.inventory_id = inventory.inventory_id
+
+left join customer 
+on rental.customer_id = customer.customer_id
+WHERE customer.first_name =  'Roberta'
