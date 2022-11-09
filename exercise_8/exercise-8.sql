3bb2736a1060b1e110bdbc99844c41c091b3642c
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-09 09:34:00 -0800

    STRETCH: exercise 8 complete

--- exercise_8/exercise-8.sql
+++ exercise_8/exercise-8.sql
@@ -1,2 +1,22 @@
 -- find all film titles rented out by customers who live in the country of Peru
 -- Similar to exercise 7 with an additional join to get country name
+SELECT title
+
+FROM film LEFT JOIN inventory
+ON film.film_id = inventory.film_id
+
+LEFT JOIN rental
+on rental.inventory_id = inventory.inventory_id
+
+LEFT JOIN customer
+ON rental.customer_id = customer.customer_id
+
+LEFT JOIN address 
+ON customer.address_id = address.address_id
+
+LEFT JOIN city
+on address.city_id = city.city_id
+
+LEFT JOIN country
+on city.country_id = country.country_id
+WHERE country = 'Peru'
