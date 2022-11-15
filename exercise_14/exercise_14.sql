596d2ac26842bf4eb8279641ae627eee3710cc8e
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-14 16:54:38 -0800

    Stretch: exercise 14 complete

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,12 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT category.name, AVG(film.length) as avg
+
+from category left join film_category
+on category.category_id = film_category.category_id
+
+LEFT JOIN film
+on film_category.film_id = film.film_id
+
+
+group by category.name
